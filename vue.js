f3aca2e93486036433a1dc17a2b16f31343be7f9
Torian <tharris@zyrobotics.com>
2019-06-06 15:17:41 -0400

    axios added

--- vue.js
+++ vue.js
@@ -1,7 +1,14 @@
-var app = new Vue({
+var url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/"
+
+new Vue({
   el: '#app',
   data: {
     newPhrase: 'HELLO WORLD',
     input: '',
+  },
+  mounted () {
+    axios
+      .get(`${url}test?key=37bc712b-dfae-495a-9dbc-8382c53e83c8`)
+      .then(response => (console.log(response)))
   }
 })
